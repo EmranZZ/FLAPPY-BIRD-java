412d87e26dd76c2efde279ee99d855b46cc59872
EMRAN AHMED <ahmed777emran@gmail.com>
2023-08-03 00:19:10 +0600

    adding Game States
    Here we add the Game States of our Flappy Bird. Game state helps us to organize the different phases of our game.  There are many types of game state like Menu state, Credit state, Actual play state.
    
    In game state, we just need to show a specific area to the users  in the world (eg. Rectangle Area). And we do that thing with a camera where Camera has  a viewport of a specific height and width.

---
+++ core/src/com/mygdx/game/States/State.java
@@ -0,0 +1,31 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+//Let's create a State of our game.
+//We can organize the different phases of our game using Game States
+public abstract class State {
+/*    //each states needs a camera to locate a position in the world
+    protected OrthographicCamera cam;
+    //creating XYZ coordinate system for mouse or some sort of pointer
+    protected Vector3 mouse;
+    //creating game state manager which is a way to manage our sates
+    //by which we can manage the functionality
+    protected GameStateManager gsm;
+
+    //generating constructor where initialize the object
+    protected State(GameStateManager gsm){
+        this.gsm = gsm;
+        cam = new OrthographicCamera();
+        mouse = new Vector3();
+    }
+
+    protected abstract void handleInput();
+    public abstract void update(float dt);
+    //where Delta time is the difference between one frame rendered and the next frame rendered
+    public abstract void render(SpriteBatch ab);
+    // where SpriteBatch renders everything to the screen in one big blob
+*/
+}
