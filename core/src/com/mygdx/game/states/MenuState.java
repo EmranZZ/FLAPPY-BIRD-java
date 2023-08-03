be60196a756e4c3b511fb2728bdbc87feef8fed3
GILMAN MD REJUAN <rejuangm@gmail.com>
2023-08-03 20:10:31 +0600

    adding Play State
    here we defined that when somebody click on the menu state how it should behave. It will transition to the play state.
    
    We rename the States package to State package as the rules of the Package Convention.
    
    Then we use setToOrtho Method to show specific portion of our game world.

--- core/src/com/mygdx/game/states/MenuState.java
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -1,5 +1,6 @@
-package com.mygdx.game.States;
+package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.FlappyDemo;
@@ -15,12 +16,15 @@ public class MenuState extends State{
 
     @Override
     public void handleInput() {
-
+        if(Gdx.input.justTouched()){ //check if the screen or any input device has been touched (i.e., a touch event has occurred) since the last frame update
+            gsm.set(new PlayState(gsm));
+            dispose();
+        }
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput(); //always checking input whether the user done anything
     }
 
     @Override
@@ -29,5 +33,11 @@ public class MenuState extends State{
         sb.draw(background, 0, 0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
         sb.draw(playBtn, (FlappyDemo.WIDTH/2)-(playBtn.getWidth()/2), FlappyDemo.HEIGHT/2);
         sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playBtn.dispose();
     }
 }
