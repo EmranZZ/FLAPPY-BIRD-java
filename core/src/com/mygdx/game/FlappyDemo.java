1c00d7d0f292254520eca1bb86f53815c0e9d7a1
EMRAN AHMED <ahmed777emran@gmail.com>
2023-08-03 11:33:43 +0600

    Game State Manager
    In this section, we tried to use Game State Manager. Basically, the Game State Manager helps to switch another Game State. Actually, The Game State Managers are essential tools for controlling the logic and data of our Flappy Bird Game.

--- core/src/com/mygdx/game/FlappyDemo.java
+++ core/src/com/mygdx/game/FlappyDemo.java
@@ -1,9 +1,13 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.States.GameStateManager;
+import com.mygdx.game.States.MenuState;
 
 public class FlappyDemo extends ApplicationAdapter {
 
@@ -11,26 +15,25 @@ public class FlappyDemo extends ApplicationAdapter {
 	public static final int WIDTH = 400;
 	public static final int HEIGHT = 800;
 	public static final String TITLE = "Flappy Bird";
-	SpriteBatch batch;
-	Texture img;
+	private GameStateManager gsm;
+	private SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1,0,0,1); //Clear Color basically wipes the screen
+		// clean and redraws everything fresh
+		
+		gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//ScreenUtils.clear(1, 0, 0, 1);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
+
 	}
 }
--- core/src/com/mygdx/game/States/State.java
+++ core/src/com/mygdx/game/States/State.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.math.Vector3;
 //Let's create a State of our game.
 //We can organize the different phases of our game using Game States
 public abstract class State {
-/*    //each states needs a camera to locate a position in the world
+    //each states needs a camera to locate a position in the world
     protected OrthographicCamera cam;
     //creating XYZ coordinate system for mouse or some sort of pointer
     protected Vector3 mouse;
@@ -25,7 +25,7 @@ public abstract class State {
     protected abstract void handleInput();
     public abstract void update(float dt);
     //where Delta time is the difference between one frame rendered and the next frame rendered
-    public abstract void render(SpriteBatch ab);
+    public abstract void render(SpriteBatch sb);
     // where SpriteBatch renders everything to the screen in one big blob
-*/
+
 }
